47b818ef75d4d78f3d49746ddcd43bee27cef544
farman13 <farman327440@gmail.com>
2026-02-14 17:19:59 +0530

    contract

--- pool.sol
+++ pool.sol
@@ -8,9 +8,12 @@ contract PoolGame is Ownable, ReentrancyGuard {
     uint256 public entryFee;
     uint256 public constant POOL_SIZE = 5;
     uint256 public constant ADMIN_FEE_PERCENT = 10;
+    address public ADMIN_WALLET;
 
     address[] public queue;
     uint256 public roundId;
+    uint256 public count;
+
 
     mapping(address => uint256) public balances;
 
@@ -24,44 +27,46 @@ contract PoolGame is Ownable, ReentrancyGuard {
     event EntryFeeUpdated(uint256 newFee);
     event Withdrawn(address indexed user, uint256 amount);
 
-    constructor(uint256 _entryFee) Ownable(msg.sender) {
+    constructor(uint256 _entryFee, address _admin) Ownable(msg.sender) {
         require(_entryFee > 0, "Invalid entry fee");
         entryFee = _entryFee;
+        ADMIN_WALLET = _admin;
         roundId = 1;
     }
 
     function joinPool() external payable {
-        require(msg.value >= entryFee, "Invalid entry fee");
+    require(msg.value >= entryFee, "Invalid entry fee");
 
-        queue.push(msg.sender);
-        emit Joined(msg.sender, roundId);
+    queue.push(msg.sender);
+    emit Joined(msg.sender, roundId);
 
-        if (queue.length >= POOL_SIZE) {
-            _selectWinner();
-        }
+    count++;
+
+    if (count == POOL_SIZE) {
+        _selectWinner();
     }
+}
 
     function _selectWinner() internal {
-        address winner = queue[0];
+    address winner = queue[0];
 
-        uint256 totalPool = entryFee * POOL_SIZE;
-        uint256 adminFee = (totalPool * ADMIN_FEE_PERCENT) / 100;
-        uint256 winnerAmount = totalPool - adminFee;
+    uint256 totalPool = entryFee * POOL_SIZE;
+    uint256 adminFee = (totalPool * ADMIN_FEE_PERCENT) / 100;
+    uint256 winnerAmount = totalPool - adminFee;
 
-        // shift queue
-        for (uint256 i = 0; i < queue.length - 1; i++) {
-            queue[i] = queue[i + 1];
-        }
-        queue.pop();
-
-        // update balances instead of sending instantly
-        balances[winner] += winnerAmount;
-        balances[owner()] += adminFee;
-
-        emit WinnerSelected(winner, winnerAmount, adminFee, roundId);
-
-        roundId++;
+    for (uint256 i = 0; i < queue.length - 1; i++) {
+        queue[i] = queue[i + 1];
     }
+    queue.pop();
+
+    balances[winner] += winnerAmount;
+    balances[ADMIN_WALLET] += adminFee;
+
+    emit WinnerSelected(winner, winnerAmount, adminFee, roundId);
+
+    count = 0;
+    roundId++;
+}
 
     function withdraw() external nonReentrant {
         uint256 amount = balances[msg.sender];
@@ -75,17 +80,24 @@ contract PoolGame is Ownable, ReentrancyGuard {
         emit Withdrawn(msg.sender, amount);
     }
 
+
     function setEntryFee(uint256 _newFee) external onlyOwner {
         require(_newFee > 0, "Fee must be > 0");
         entryFee = _newFee;
         emit EntryFeeUpdated(_newFee);
     }
 
+    function setAdminWallet(address _newAdmin) external onlyOwner {
+    require(_newAdmin != address(0), "Invalid admin");
+    ADMIN_WALLET = _newAdmin;
+}
+
+
     function getQueue() external view returns (address[] memory) {
         return queue;
     }
 
-    function queueLength() external view returns (uint256) {
-        return queue.length;
+    function poolLength() external view returns (uint256) {
+        return count;
     }
 }
